9629c47daefacc33f536e4162c312751885f087e
mast3r123 <abhishek.mast3r@gmail.com>
2022-07-29 17:08:40 -0400

    Add setup and tear down with refactoring

--- CashRegister.playground/Contents.swift
+++ CashRegister.playground/Contents.swift
@@ -2,13 +2,33 @@ import XCTest
 
 class CashRegister {
     var availableFunds: Decimal
+    var transactionTotal: Decimal = 0
     
     init(availableFunds: Decimal) {
         self.availableFunds = availableFunds
     }
+    
+    func addItem(_ cost: Decimal) {
+        transactionTotal = cost
+    }
 }
 
 class CashRegisterTest: XCTestCase {
+    
+    var availableFunds: Decimal!
+    var sut: CashRegister!
+    
+    override func setUp() {
+        super.setUp()
+        availableFunds = Decimal(100)
+        sut = CashRegister(availableFunds: availableFunds)
+    }
+    
+    override func tearDown() {
+        availableFunds = nil
+        sut = nil
+        super.tearDown()
+    }
     
     func testInitAvailableFunds_setAvailableFunds() {
         //given
@@ -19,6 +39,21 @@ class CashRegisterTest: XCTestCase {
         
         //then
         XCTAssertEqual(sut.availableFunds, availableFunds)
+    }
+    
+    func testAddItem_oneItem_addCostsToTransactionTotal() {
+        //given
+        let availableFunds = Decimal(100)
+        let sut = CashRegister(availableFunds: availableFunds)
+        
+        let item = Decimal(42)
+        
+        //when
+        sut.addItem(item)
+        
+        //then
+        XCTAssertEqual(sut.transactionTotal, item)
+        
     }
 }
 
